c6157cffd90e4722836c53df1d670e9ba48816fd
Ella Dawson <94555843+dawsonellab@users.noreply.github.com>
2025-09-24 21:57:28 -0400

    Add files via upload

---
+++ heart_rate.py
@@ -0,0 +1,8 @@
+# heart_rate.py
+class HeartRateMonitor:
+    def __init__(self):
+        self.current_rate = 0
+
+    def get_heart_rate(self):
+        # For now, return a dummy value
+        return 80
